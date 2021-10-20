e66153cf4543c3f7e82c24e58287d42f9f138f56
@0Melon0 <576788380@qq.com>
2021-10-20 18:39:12 +0800

    fix2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { RawSource } = require('webpack-sources');
 const Fontmin = require('fontmin');
+const { option } = require('../demo/node_modules/commander/typings');
 const commonWord = "1234567890-=！@#￥%……&*()~:\"{}[]|\\?/<>,.;'+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ〔〕的一是在不了有和人这中大为上个国我以要他时来用们生到作地于出就分对成会可主发年动同工也能下过子说产种面而方后多定行学法所民得经十三之进着等部度家电力里如水化高自二理起小物现实加量都两体制机当使点从业本去把性好应开它合还因由其些然前外天政四日那社义事平形相全表间样" +
   "与关各重新线内数正心反你明看原又么利比或但质气第向道命此变条只没结解问意建月公无系军很情者最立代想已通并提直题党程展五果料象员革位入常文总次品式活设及管特件长求老头基资边流路级少图山统接知较长将组见计别她手角期根论运农指几九区强放决西被干做必战先回则任取据处队南给色光门即保治北造百规热领七海地口东导器压志" +
   "世金增争济阶油思术极交受联什认六共权收证改清已美再采转更单风切打白教速花带安场身车例真务具万每目至达走积示议声报斗完类八离华名确才科张信马节话米整空元况今集温传土许步群广石记需段研界拉林律叫且究观越织装影算低持音众书布复容儿须际商非验连断深难近矿千周委素技备半办青省列习响约支般史感劳便团往酸历市克何除消构府称太准精值" +
@@ -24,10 +25,12 @@ module.exports = class MelonMinFontPlugin {
   constructor(options) {
     this.exclude = [];
     if (options) {
-      if (options.isMerge) {
+      if (options.isMerge && options.extraWord) {
         this.extraWord = options.extraWord + commonWord;
-      } else {
+      } else if (options.extraWord) {
         this.extraWord = options.extraWord;
+      } else {
+        this.extraWord = commonWord;
       }
       this.exclude = Array.isArray(options.exclude) ? options.exclude : [];
     } else {
